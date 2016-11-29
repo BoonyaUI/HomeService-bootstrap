398437849a674026a00bdca0f410bbfffdb90ff3
unknown <pengjunlin@wlyd.local>
2016-11-29 18:49:26 +0800

    Commit all codes!

---
+++ js/settings.js
@@ -0,0 +1,8 @@
+/**
+ *应用配置属性 
+ */
+var PROJECT_SETTING={PROJECT_NAME:"生活小猪猪",PROJECT_VERSION:"1.0.0"};
+
+$(document).ready(function(){
+	  $(document).attr("title",PROJECT_SETTING.PROJECT_NAME);
+});
